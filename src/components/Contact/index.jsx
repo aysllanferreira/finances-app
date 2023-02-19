95709413c469d9aa57bdb5292139b57c40214844
Aysllan Ferreira <aysllan.ferreiraks@gmail.com>
2023-02-19 12:24:39 -0300

    feat: Contact

--- src/components/Contact/index.jsx
+++ src/components/Contact/index.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { AnimationOnScroll } from 'react-animation-on-scroll';
 import './Contact.scss';
 
 function Contact() {
@@ -8,38 +9,52 @@ function Contact() {
   return (
     <div id="contact" className="Contact">
       <div className="container Contact__content">
-        <h2 className="Contact__content__title">Contact</h2>
-        <form className="Contact__content__form">
-          <div className="Contact__content__form__group">
-            <label htmlFor="name">
-              Name
-              {' '}
-            </label>
-            <input type="text" name="name" id="name" />
-          </div>
-          <div className="Contact__content__form__group">
-            <label htmlFor="email">
-              Email
-              {' '}
-            </label>
-            <input type="email" name="email" id="email" />
+        <AnimationOnScroll
+          animateIn="animate__flipInY"
+          animateOut="animate__flipOutY"
+          animateOnce
+          delay={100}
+        >
+          <h2 className="Contact__content__title">Contact</h2>
+        </AnimationOnScroll>
 
-          </div>
-          <div className="Contact__content__form__group">
-            <label htmlFor="message">
-              Message
-              {' '}
-            </label>
-            <textarea name="message" id="message" cols="30" rows="10" />
-          </div>
-          <button
-            type="submit"
-            className="Contact__content__form__button"
-            onClick={handleClick}
-          >
-            Send
-          </button>
-        </form>
+        <AnimationOnScroll
+          animateIn="animate__lightSpeedInLeft"
+          animateOnce
+          delay={100}
+        >
+          <form className="Contact__content__form">
+            <div className="Contact__content__form__group">
+              <label htmlFor="name">
+                Name
+                {' '}
+              </label>
+              <input type="text" name="name" id="name" />
+            </div>
+            <div className="Contact__content__form__group">
+              <label htmlFor="email">
+                Email
+                {' '}
+              </label>
+              <input type="email" name="email" id="email" />
+
+            </div>
+            <div className="Contact__content__form__group">
+              <label htmlFor="message">
+                Message
+                {' '}
+              </label>
+              <textarea name="message" id="message" cols="30" rows="10" />
+            </div>
+            <button
+              type="submit"
+              className="Contact__content__form__button"
+              onClick={handleClick}
+            >
+              Send
+            </button>
+          </form>
+        </AnimationOnScroll>
       </div>
     </div>
   );
