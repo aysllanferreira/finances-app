767c2357ef3e3a499460a95a2ba72c3892a651b0
Aysllan Ferreira <aysllan.ferreiraks@gmail.com>
2023-02-19 12:22:47 -0300

    feat: Testimonial

--- src/components/Testimonial/index.jsx
+++ src/components/Testimonial/index.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import { AnimationOnScroll } from 'react-animation-on-scroll';
 import { GrCaretNext, GrCaretPrevious } from 'react-icons/gr';
 import testimonials from '../../constants/testimonials';
 import './Testimonial.scss';
@@ -19,50 +20,64 @@ function Testimonial() {
   return (
     <div className="Testimonial" id="testimonial">
       <div className="Testimonial__content">
-        <h1 className="Testimonial__content__title">Testimonials</h1>
-        <div className="Englobation">
-          <button
-            type="button"
-            className="Testimonial__content__caroulsel__button"
-            onClick={handlePrevious}
-          >
-            <GrCaretPrevious />
-          </button>
-          <div className={`Testimonial__content__caroulsel ${direction === 'next' ? 'goNext' : 'goPrev'}`}>
-            {testimonials.map((testimonial, index) => (
-              <div
-                className={`Testimonial__content__caroulsel__item slide ${
-                  index === current ? 'current ' : ''
-                }${current === 0 && index === testimonials.length - 1 ? 'previous ' : ''}${
-                  current === testimonials.length - 1 && index === 0 ? 'next ' : ''
-                }${current > index ? 'previous ' : ''}${current < index ? 'next' : ''}`}
-                key={testimonial.name}
-              >
-                <div className="Testimonial__content__caroulsel__item__content">
-                  <img
-                    src={testimonial.img}
-                    alt="Testimonial"
-                    className="Testimonial__content__caroulsel__item__content__image"
-                  />
-                  <h2 className="Testimonial__content__caroulsel__item__content__name">
-                    <span>{testimonial.name}</span>
-                  </h2>
+        <AnimationOnScroll
+          animateIn="animate__flipInY"
+          animateOut="animate__flipOutY"
+          animateOnce
+          delay={100}
+        >
+          <h1 className="Testimonial__content__title">Testimonials</h1>
+        </AnimationOnScroll>
 
-                  <p className="Testimonial__content__caroulsel__item__content__position">{testimonial.position}</p>
+        <AnimationOnScroll
+          animateIn="animate__flipInX"
+          animateOnce
+          delay={100}
+        >
+          <div className="Englobation">
+            <button
+              type="button"
+              className="Testimonial__content__caroulsel__button"
+              onClick={handlePrevious}
+            >
+              <GrCaretPrevious />
+            </button>
+            <div className={`Testimonial__content__caroulsel ${direction === 'next' ? 'goNext' : 'goPrev'}`}>
+              {testimonials.map((testimonial, index) => (
+                <div
+                  className={`Testimonial__content__caroulsel__item slide ${
+                    index === current ? 'current ' : ''
+                  }${current === 0 && index === testimonials.length - 1 ? 'previous ' : ''}${
+                    current === testimonials.length - 1 && index === 0 ? 'next ' : ''
+                  }${current > index ? 'previous ' : ''}${current < index ? 'next' : ''}`}
+                  key={testimonial.name}
+                >
+                  <div className="Testimonial__content__caroulsel__item__content">
+                    <img
+                      src={testimonial.img}
+                      alt="Testimonial"
+                      className="Testimonial__content__caroulsel__item__content__image"
+                    />
+                    <h2 className="Testimonial__content__caroulsel__item__content__name">
+                      <span>{testimonial.name}</span>
+                    </h2>
 
-                  <p className="Testimonial__content__caroulsel__item__content__description">{testimonial.text}</p>
+                    <p className="Testimonial__content__caroulsel__item__content__position">{testimonial.position}</p>
+
+                    <p className="Testimonial__content__caroulsel__item__content__description">{testimonial.text}</p>
+                  </div>
                 </div>
-              </div>
-            ))}
+              ))}
+            </div>
+            <button
+              type="button"
+              className="Testimonial__content__caroulsel__button"
+              onClick={handleNext}
+            >
+              <GrCaretNext />
+            </button>
           </div>
-          <button
-            type="button"
-            className="Testimonial__content__caroulsel__button"
-            onClick={handleNext}
-          >
-            <GrCaretNext />
-          </button>
-        </div>
+        </AnimationOnScroll>
       </div>
     </div>
   );
