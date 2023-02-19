de0d65277c5ec5d67d06cb4e25e1f4175cfb6215
Aysllan Ferreira <aysllan.ferreiraks@gmail.com>
2023-02-19 12:17:06 -0300

    feat: Animation CTO

--- src/components/CTO/index.jsx
+++ src/components/CTO/index.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { AnimationOnScroll } from 'react-animation-on-scroll';
 import './CTO.scss';
 
 function CTO() {
@@ -8,41 +9,60 @@ function CTO() {
   };
   return (
     <section className="CTO">
-      <div className="CTO__img">
-        <div className="CTO__content">
-          <h2 className="CTO__content__title">
-            To become a
-            {' '}
-            <span>successful</span>
-            {' '}
-            person, you must first have
-            {' '}
-            <span>control</span>
-            {' '}
-            of your
-            {' '}
-            <span>financial life</span>
-            ,
-            and then take
-            {' '}
-            <span>decisions</span>
-            {' '}
-            for bigger steps.
-            <br />
-            {' '}
-            This is
-            {' '}
-            <span>crucial</span>
-            .
-          </h2>
-          <button
-            type="button"
-            onClick={() => scrollTo('pricing')}
-          >
-            Get Started
-          </button>
+      <AnimationOnScroll
+        animateIn="animate__fadeIn"
+        animateOnce
+        delay={100}
+      >
+        <div className="CTO__img">
+          <div className="CTO__content">
+            <AnimationOnScroll
+              animateIn="animate__fadeInDown"
+              animateOnce
+              delay={100}
+            >
+              <h2 className="CTO__content__title">
+                To become a
+                {' '}
+                <span>successful</span>
+                {' '}
+                person, you must first have
+                {' '}
+                <span>control</span>
+                {' '}
+                of your
+                {' '}
+                <span>financial life</span>
+                ,
+                and then take
+                {' '}
+                <span>decisions</span>
+                {' '}
+                for bigger steps.
+                <br />
+                {' '}
+                This is
+                {' '}
+                <span>crucial</span>
+                .
+              </h2>
+            </AnimationOnScroll>
+
+            <AnimationOnScroll
+              animateIn="animate__fadeInUp"
+              animateOnce
+              delay={200}
+            >
+              <button
+                type="button"
+                onClick={() => scrollTo('pricing')}
+              >
+                Get Started
+              </button>
+            </AnimationOnScroll>
+          </div>
         </div>
-      </div>
+      </AnimationOnScroll>
     </section>
   );
 }
